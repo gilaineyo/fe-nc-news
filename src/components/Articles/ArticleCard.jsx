a897a2c456a73581f48edb80f73a1c4ad3fc01cf
Gilaine Young <gilaineyoung@gmail.com>
2023-12-11 14:24:07 +0000

    Adds articles list functionality and styling for ease of development

--- src/components/Articles/ArticleCard.jsx
+++ src/components/Articles/ArticleCard.jsx
@@ -1,9 +1,21 @@
 import './ArticleCard.css'
+import { getArticles } from '../../utils/utils'
 
-const ArticleCard = () => {
+const ArticleCard = ({articles}) => {
     return (
         <div className='article-card'>
-            <h3>ArticleCard</h3>
+            <ul>
+            {articles.map((article) => {
+                const { author, title, article_id, article_img_url, comment_count, votes, topic, created_at } = article
+                return (
+                    <li className="card" key={article_id}>
+                        <img src={article_img_url} alt="article image" />
+                        <h4>{title}</h4>
+                        <p>{author}</p>
+                    </li>
+                )
+            })}
+            </ul>
         </div>
     )
 }
--- src/components/Articles/Articles.jsx
+++ src/components/Articles/Articles.jsx
@@ -1,14 +1,25 @@
+import { useEffect, useState } from 'react'
 import ArticleCard from './ArticleCard'
 import './Articles.css'
 import Filters from './Filters'
 import PostArticle from './PostArticle'
+import { getArticles } from '../../utils/utils'
 
 const Articles = () => {
+    const [articles, setArticles] = useState([])
+    
+    useEffect(() => {
+        getArticles()
+        .then((articles) => {
+            setArticles(articles)
+        })
+    }, [])
+
     return (
         <div className='articles'>
             <h2>Articles</h2>
             <Filters />
-            <ArticleCard />
+            <ArticleCard articles={articles} />
             <PostArticle />
         </div>
     )
