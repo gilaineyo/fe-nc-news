f3d5a3a4daa6b97854ce59c5d3b2a1b43cacfc74
Gilaine Young <gilaineyoung@gmail.com>
2023-12-13 09:17:14 +0000

    Adds conditional rendering of delete button for comment depending on if username on comment matches logged in user

--- src/components/Articles/ArticleFocus/Comments/CommentCard.jsx
+++ src/components/Articles/ArticleFocus/Comments/CommentCard.jsx
@@ -1,25 +1,33 @@
 import { getUserDetails } from "../../../../utils/utils"
-import { useEffect, useState } from "react"
+import { useEffect, useState, useContext } from "react"
 import './CommentCard.css'
+import { UserContext } from "../../../../contexts/UserContext"
 
 const CommentCard = ({comment}) => {
-    const [user, setUser] = useState({})
+    const [authorProfile, setAuthorProfile] = useState({})
+    const {user} = useContext(UserContext)
     const { author, body, votes, created_at } = comment
     const postedAt = new Date(created_at)
 
     useEffect(() => {
         getUserDetails(author)
-        .then((user) => {
-            setUser(user)
+        .then((validUser) => {
+            setAuthorProfile(validUser)
         })
     }, [])
+    
+    console.log(user, "<--- user")
+    console.log(authorProfile, "<--- userProfile")
 
     return (
         <div className="comment-card">
-            <img className='avatar' src={user.avatar_url} alt="user avatar" />
+            <img className='avatar' src={authorProfile.avatar_url} alt="user avatar" />
             <p className='posted-details'>{author}, {postedAt.toLocaleString()}</p>
             <p className='comment-body'>{body}</p>
             <button className='upvote'>Votes: {votes}</button>
+            {authorProfile.username === user.username 
+                ? <button className="delete">Delete</button> 
+                : null}
         </div>
     )
 }
